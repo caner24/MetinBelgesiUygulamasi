5529e81a638ae8e6fdb9c959c1ec6ce21d28a418
Caner Ay Celep <72454642+caner24@users.noreply.github.com>
2021-04-23 22:14:43 +0300

    Add project files.

---
+++ MetinBelgesi/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace MetinBelgesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dosyaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yeniDosyaOluşturToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.açToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kaydetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.farklıYereKaydetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yazıTipiAyarlarıToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yazıTipiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 422);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaToolStripMenuItem,
+            this.yazıTipiAyarlarıToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            this.dosyaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yeniDosyaOluşturToolStripMenuItem,
+            this.açToolStripMenuItem,
+            this.kaydetToolStripMenuItem,
+            this.farklıYereKaydetToolStripMenuItem});
+            this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.dosyaToolStripMenuItem.Text = "Dosya";
+            // 
+            // yeniDosyaOluşturToolStripMenuItem
+            // 
+            this.yeniDosyaOluşturToolStripMenuItem.Name = "yeniDosyaOluşturToolStripMenuItem";
+            this.yeniDosyaOluşturToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.yeniDosyaOluşturToolStripMenuItem.Text = "Yeni Dosya Oluştur";
+            this.yeniDosyaOluşturToolStripMenuItem.Click += new System.EventHandler(this.yeniDosyaOluşturToolStripMenuItem_Click);
+            // 
+            // açToolStripMenuItem
+            // 
+            this.açToolStripMenuItem.Name = "açToolStripMenuItem";
+            this.açToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.açToolStripMenuItem.Text = "Aç";
+            this.açToolStripMenuItem.Click += new System.EventHandler(this.açToolStripMenuItem_Click);
+            // 
+            // kaydetToolStripMenuItem
+            // 
+            this.kaydetToolStripMenuItem.Name = "kaydetToolStripMenuItem";
+            this.kaydetToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.kaydetToolStripMenuItem.Text = "Kaydet";
+            this.kaydetToolStripMenuItem.Click += new System.EventHandler(this.kaydetToolStripMenuItem_Click);
+            // 
+            // farklıYereKaydetToolStripMenuItem
+            // 
+            this.farklıYereKaydetToolStripMenuItem.Name = "farklıYereKaydetToolStripMenuItem";
+            this.farklıYereKaydetToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.farklıYereKaydetToolStripMenuItem.Text = "Farklı Yere Kaydet";
+            this.farklıYereKaydetToolStripMenuItem.Click += new System.EventHandler(this.farklıYereKaydetToolStripMenuItem_Click);
+            // 
+            // yazıTipiAyarlarıToolStripMenuItem
+            // 
+            this.yazıTipiAyarlarıToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yazıTipiToolStripMenuItem});
+            this.yazıTipiAyarlarıToolStripMenuItem.Name = "yazıTipiAyarlarıToolStripMenuItem";
+            this.yazıTipiAyarlarıToolStripMenuItem.Size = new System.Drawing.Size(104, 20);
+            this.yazıTipiAyarlarıToolStripMenuItem.Text = "Yazı Tipi Ayarları";
+            // 
+            // yazıTipiToolStripMenuItem
+            // 
+            this.yazıTipiToolStripMenuItem.Name = "yazıTipiToolStripMenuItem";
+            this.yazıTipiToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.yazıTipiToolStripMenuItem.Text = "Yazı Tipi";
+            this.yazıTipiToolStripMenuItem.Click += new System.EventHandler(this.yazıTipiToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yeniDosyaOluşturToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem açToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kaydetToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem yazıTipiAyarlarıToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yazıTipiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem farklıYereKaydetToolStripMenuItem;
+    }
+}
+
---
+++ MetinBelgesi/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MetinBelgesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // ******************************************************** Developed by caner24 ********************************************************************* //
+
+        // Streamwriter ve Streamreader methodlarımınızın veriyi yazabilmesi ve çekebilmesi için string yol değişkenini tanımladık
+        string yol;
+
+        // Yazı yazdığımız durumlarda bir değişikliğin olduğunu bildirmek için formun text'inde uyarı amacıyla yazı yazmak için bu int sayac değişkeni oluşturuldu.
+        int sayac;
+        private void kaydetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            // Dosyayı kaydederken bir hata olursa kullanıcıya bir mesaj vermesi için try-catch bloğu kulanıldı
+            try
+            {
+                sayac = 0;
+                StreamWriter yaz = new StreamWriter(yol);
+                yaz.WriteLine(richTextBox1.Text);
+                this.Text = "Form1";
+                yaz.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lütfen önce dosyanın yolunu bulmak için dosyayı farklı kaydeti seçiniz ");
+                sayac = 1;
+            }
+        }
+
+        private void yazıTipiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Kullanici yazı stili ve yazı kalınlığını değiştirmek isterse değişikliği yapabilmesi için FontDialog koyuldu.
+            FontDialog dialog = new FontDialog();
+            DialogResult cevap = dialog.ShowDialog();
+            if (cevap == DialogResult.OK)
+            {
+                richTextBox1.Font = dialog.Font;
+            }
+        }
+        private void açToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Richtextbox'ın textchanged özelliği ile eğer richtextbox'a birşey yazılırsa sayacın değerini değiştirerek formun textine yazı yazılması için sayaç değişkeni değeri atandı.
+
+            if (sayac == 1)
+            {
+                // Eğer kullanıcı yazıyı kaydetmeden bir başka metin belgesini açmak istiyorsa kullanıcıya uyarı amaçlı bir metin yansıtıldı
+                DialogResult cevap = MessageBox.Show("Yazılan Metni Kaydetmediniz,Kaydetmek için EVET ' e basınız ", "Kaydedilmeyen Metin", MessageBoxButtons.YesNo);
+                if (cevap == DialogResult.Yes)
+                {
+                    // Kaydetme işlemleri için saveFileDialog1 nesnesi kullanıldı 
+                    saveFileDialog1.Title = "Kayit Yerini Seçin";
+                    saveFileDialog1.Filter = "Metin Belgesi (*.txt) | *.txt";
+                    saveFileDialog1.FilterIndex = 2;
+                    saveFileDialog1.ShowDialog();
+                    yol = saveFileDialog1.FileName;
+                    StreamWriter yaz = new StreamWriter(saveFileDialog1.FileName);
+                    yaz.WriteLine(richTextBox1.Text);
+                    yaz.Close();
+                }
+                // Eğer kullanıcı kaydetmek istemiyorsa sayac tekrardan 0 'a eşitlendi
+                if (cevap == DialogResult.No)
+                {
+                    sayac = 0;
+                }
+            }
+            // Richtextbox'ın textchanged özelliği ile eğer richtextbox'a birşey yazılırsa sayacın değerini değiştirerek formun textine yazı yazılması için sayaç değişkeni değeri atandı.
+            if (sayac == 0)
+            {
+                // Kullanici açacağı dosyayı seçmesi için OpenFileDialog nesnesi tanımlandı
+                OpenFileDialog metniac = new OpenFileDialog();
+                metniac.Filter = "Metin Dosyası(*.txt) |*.txt";
+                // Kullanici çoklu seçim yapmaması için çoklu seçim özelliği devre dışı bırakıldı
+                metniac.Multiselect = false;
+                if (metniac.ShowDialog() == DialogResult.OK)
+                {
+                    // Openfiledialog ile açtığımız dosyanın yolunu string nesnesine aktardık.
+                    yol = metniac.FileName;
+                    // Bir hata olduğunda bildirmek için try-catch bloğu kullanıldı
+                    try
+                    {
+                        // Kullanici halihazirda olan bir txt metin belgesini açmak isterse StreamReader aracı ile ilgili işlemler yapıldı
+                        StreamReader oku = new StreamReader(metniac.FileName);
+                        richTextBox1.Text = oku.ReadToEnd();
+                        oku.Close();
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Bu Dosyayı Okurken Bir Hata Oluştu");
+                    }
+                }
+            }
+        }
+
+        private void yeniDosyaOluşturToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Yeni dosya oluştuğu zaman kaydetmemiz için gerekli olan yol değişkeni de yeni sayfaya göre hazırlanması için sıfırlandı.
+            yol = "";
+            if (sayac == 1)
+            {
+                // Eğer kullanıcı yazıyı kaydetmeden bir başka metin belgesini açmak istiyorsa kullanıcıya uyarı amaçlı bir metin yansıtıldı
+                DialogResult cevap = MessageBox.Show("Yazılan Metni Kaydetmediniz,Kaydetmek için EVET ' e basınız ", "Kaydedilmeyen Metin", MessageBoxButtons.YesNo);
+                if (cevap == DialogResult.Yes)
+                {
+                    // Kaydetme işlemleri için saveFileDialog1 nesnesi kullanıldı 
+                    saveFileDialog1.Title = "Kayit Yerini Seçin";
+                    saveFileDialog1.Filter = "Metin Belgesi (*.txt) | *.txt";
+                    saveFileDialog1.FilterIndex = 2;
+                    saveFileDialog1.ShowDialog();
+                    yol = saveFileDialog1.FileName;
+                    StreamWriter yaz = new StreamWriter(saveFileDialog1.FileName);
+                    yaz.WriteLine(richTextBox1.Text);
+                    yaz.Close();
+                }
+                if (cevap == DialogResult.No)
+                {
+                    sayac = 0;
+                }
+            }
+            richTextBox1.Clear();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Sayaç değişkeni richtextbox'da herhangi bir değişiklik olduğunda arttırılarak kullanıcıya görsel uyarı vermesi amacıyla '*' yazısı eklendi.
+            sayac = 1;
+            if (sayac == 1)
+            {
+                this.Text = "Form1" + "" + " * ";
+            }
+            // Sayaç değişkeni richtextbox'da herhangi bir değişiklik olmadığında kullanıcıya görsel uyarı vermesi amacıyla '*' yazısı kaldırılarak direk uygulama yazısı yazdırıldı.
+            if (richTextBox1.Text == "")
+            {
+                sayac = 0;
+                this.Text = "Form1";
+            }
+        }
+
+        private void farklıYereKaydetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Eğer kullanici açtiği metin belgesini farklı bir yere kaydetmek isterse savefiledialog nesnesi kullanılarak gerekli işlemler yapıldı
+            try
+            {
+                // Sayac nesnesindeki değişiklikler yapıldı.Kullanıcı yazılarda yapmış olduğu değişikliği kaydetmeyi seçtiği için sıfırlandı.
+                sayac = 0;
+                saveFileDialog1.Title = "Kayit Yerinizi Seçin";
+                saveFileDialog1.Filter = "Metin Dosyası(*.txt) | *.txt";
+                saveFileDialog1.FilterIndex = 2;
+                saveFileDialog1.ShowDialog();
+                yol = saveFileDialog1.FileName;
+                StreamWriter kaydet = new StreamWriter(saveFileDialog1.FileName);
+                kaydet.WriteLine(richTextBox1.Text);
+                kaydet.Close();
+                this.Text = "Form1";
+                MessageBox.Show("Kaynak Metne Yazdırıldı");
+
+            }
+            catch (Exception)
+            {
+                // Herhangi bir hata gelirse sayaç tekrardan 1 yapılarak kullanıcıya kaydedilmediğini göstermek için uyarı yapıldı
+                sayac = 1;
+                MessageBox.Show("Hata Oluştu Metin Yazdırılamadı");
+            }
+        }
+    }
+}
